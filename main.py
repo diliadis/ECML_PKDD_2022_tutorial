804d287947481d212f07d0f529cafc685a342e52
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-09-14 12:14:52 +0000

    small change

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def load_dataset(dataset_name, root_dir):
     return Y, X1, X2
 
 
-def get_mystery_dataset(dataset_name, root_dir, mode=0):
+def get_mystery_dataset_v2(dataset_name, root_dir, mode=0):
     valid_modes = [0, 1, 2]
     if mode not in valid_modes:
         raise AttributeError('Please use one of the valid modes: '+str(valid_modes))
@@ -25,19 +25,18 @@ def get_mystery_dataset(dataset_name, root_dir, mode=0):
 
     triplets = [(i, j, Y[i, j]) for i in range(Y.shape[0]) for j in range(Y.shape[1])]
     if mode in [0, 1]:
-        train_instance_ids, test_instance_ids = train_test_split(range(Y.shape[0]), test_size=0.25, shuffle=False, random_state=42)
-        train_triplets = [i for i in triplets if i[0] in train_instance_ids]
-        test_triplets = [i for i in triplets if i[0] in test_instance_ids]
-
-        train_df = pd.DataFrame(train_triplets, columns =['instance_id', 'target_id', 'value'])
-        test_df = pd.DataFrame(test_triplets, columns =['instance_id', 'target_id', 'value'])
+        Y_train, Y_test, X1_train, X1_test = train_test_split(Y, X1, test_size=0.25, shuffle=False, random_state=42)
     else:
         train_instance_ids, test_instance_ids = train_test_split(range(Y.shape[0]), test_size=0.25, shuffle=False, random_state=42)
         train_target_ids, test_target_ids = train_test_split(range(Y.shape[1]), test_size=0.25, shuffle=False, random_state=42)    
-        train_triplets = [i for i in triplets if i[0] in train_instance_ids and i[1] in train_target_ids]
-        test_triplets = [i for i in triplets if i[0] in test_instance_ids and i[1] in test_target_ids]      
+
+        Y_train, Y_test = Y[train_instance_ids, :][:, train_target_ids], Y[test_instance_ids, :][:, test_target_ids]
+        X1_train, X1_test = X1[train_instance_ids, :], X1[test_instance_ids, :]
+        X2_train, X2_test = X2[train_target_ids, :], X2[test_target_ids, :]
 
     if mode == 0:
-        return train_df, test_df, X1
+        return Y_train, Y_test, X1_train, X1_test
+    elif mode == 1:
+        return Y_train, Y_test, X1_train, X1_test, X2
     else:
-        return train_df, test_df, X1, X2
+        return Y_train, Y_test, X1_train, X1_test, X2_train, X2_test
